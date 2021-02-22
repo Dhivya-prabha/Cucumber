a78f95bf6a1721c7353138162fdd34aaece3a3f3
Dhivya-prabha <49254873+Dhivya-prabha@users.noreply.github.com>
2021-02-22 19:42:11 +0530

    Add files via upload

---
+++ src/main/java/steps/Hooks.java
@@ -0,0 +1,30 @@
+package steps;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import steps.Baseclass;
+
+public class Hooks extends Baseclass{
+
+	@Before
+	public void precondition() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.findElementById("username").sendKeys("demosalesmanager");
+		driver.findElementById("password").sendKeys("crmsfa");
+		driver.findElementByClassName("decorativeSubmit").click();
+		driver.findElementByLinkText("CRM/SFA").click();
+		driver.findElementByLinkText("Leads").click();
+
+	}
+	@After
+	public void postcondition() {
+		driver.close();
+	}
+	}
+
